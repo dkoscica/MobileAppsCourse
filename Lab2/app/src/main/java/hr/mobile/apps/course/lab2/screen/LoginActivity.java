487610afc0d764d6a68701414ecbb3b2fd89df26
dkoscica <dominik.koscica@gmail.com>
2019-03-20 22:06:44 +0100

    Add Lab2

---
+++ Lab2/app/src/main/java/hr/mobile/apps/course/lab2/screen/LoginActivity.java
@@ -0,0 +1,15 @@
+package hr.mobile.apps.course.lab2.screen;
+
+import androidx.appcompat.app.AppCompatActivity;
+import hr.mobile.apps.course.lab2.R;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
